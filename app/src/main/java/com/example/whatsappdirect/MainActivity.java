116f2192d3743d07c307b7e760cbf1f5202a20dd
harshil <harshilpadsala999@gmail.com>
2022-07-28 21:00:56 +0530

    Major Change in GUI and also update the ui components

--- app/src/main/java/com/example/whatsappdirect/MainActivity.java
+++ app/src/main/java/com/example/whatsappdirect/MainActivity.java
@@ -1,39 +1,41 @@
 package com.example.whatsappdirect;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
 
-    CountryCodePicker ccp;
-    EditText number;
-    Button button;
+    CountryCodePicker countryCodePicker;
+    TextInputEditText mobileNumber;
+    Button chatBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        number = findViewById(R.id.number);
-        button = findViewById(R.id.button);
-        ccp = findViewById(R.id.ccp);
+        // finding id's of all components
+        countryCodePicker = findViewById(R.id.countryCodePicker);
+        mobileNumber = findViewById(R.id.editMobileNumber);
+        chatBtn = findViewById(R.id.chatBtn);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        chatBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String code = ccp.getSelectedCountryCode();
-                String s = number.getText().toString();
+                String code = countryCodePicker.getSelectedCountryCode();
+                String s = mobileNumber.getEditableText().toString();
                 String done = code+s;
-                Toast.makeText(MainActivity.this, "procssing", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(MainActivity.this, "processing", Toast.LENGTH_SHORT).show();
                 gotoUrl("https://wa.me/+"+done);
             }
         });
